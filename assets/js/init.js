b670eae8648c1d286476ab3d047b518e595c0513
Dave Widmer <dwidmer@bgsu.edu>
2012-07-19 15:08:32 -0400

    Updating the theme and init file.

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,5 +2,13 @@
  * Initialization for the Slide Deck
  */
 (function($){
-	$.deck('.slide');
+	$.deck('.slide', {
+		selectors: {
+			container: 'body > article'
+		}
+	});
+
+	$('pre code').each( function(i, e){
+		hljs.highlightBlock(e);
+	});
 })(jQuery);
