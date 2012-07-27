90d0ed630399a047b20ffbc21c2a1016572c5f44
Dave Widmer <dwidmer@bgsu.edu>
2012-07-27 12:55:24 -0400

    First pages commit

---
+++ assets/js/init.js
@@ -0,0 +1,18 @@
+/**
+ * Initialization for the Slide Deck
+ */
+(function($){
+	$.deck('.slide', {
+		selectors: {
+			container: 'body > article'
+		}
+	});
+
+	$('pre code').each( function(i, e){
+		hljs.highlightBlock(e);
+	});
+
+	$.livecode({
+		callback: hljs.highlightBlock
+	});
+})(jQuery);
